f09282024590c14c9124e474255972fdf8c6794d
Olexandr-Sliusar <103962458+Olexandr-Sliusar@users.noreply.github.com>
2022-10-15 13:20:41 +0000

    Deploying to gh-pages from @ Olexandr-Sliusar/goit-js-hw-09@fe928accf66aa42aa26979251f2f479d046dbc51 🚀

---
+++ 01-color-switcher.7e4473a0.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};window.onload=void(t.stopBtn.disabled=!0),t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(e)}));let e=null;
+//# sourceMappingURL=01-color-switcher.7e4473a0.js.map
